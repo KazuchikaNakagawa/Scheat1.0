38e1c659d00fce0c65edafe3d84268873f7c527d
KazuchikaNakagawa <62341618+KazuchikaNakagawa@users.noreply.github.com>
2020-07-30 21:30:39 +0900

    iMac

--- TestScheat/main.cpp
+++ TestScheat/main.cpp
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     scheat::Scheat sch = scheat::Scheat();
     sch.FatalError("Use this as a test.", __LINE__);
+    printf("this is iMac text;\n");
     return 0;
 }
