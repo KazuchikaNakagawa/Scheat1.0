229f011160e8b96199177e11ef37b779556251b6
KazuchikaNakagawa <62341618+KazuchikaNakagawa@users.noreply.github.com>
2020-07-31 10:47:47 +0900

    coding...

--- scheat/Lexer.hpp
+++ scheat/Lexer.hpp
@@ -9,5 +9,56 @@
 #define Lexer_hpp
 
 #include <stdio.h>
+#include "scheat.hpp"
+#include <fstream>
+
+enum TokenKind {
+    val_identifier,
+    val_num,
+    val_str,
+    val_double,
+    
+    tok_this,
+    tok_that,
+    tok_is,
+    tok_external,
+};
+
+union TokenValue {
+    std::string strValue;
+    int intValue;
+    bool boolValue;
+    double doubleValue;
+};
+
+struct Token {
+    Token *next;
+    Token *prev;
+    TokenKind kind;
+    TokenValue value;
+    Token *last();
+    Token *first();
+    
+};
+
+enum LexerState {
+    commentState,
+    numberState,
+    doubleState,
+    stringState,
+    identifierState,
+    initState
+};
+
+class Lexer {
+    std::string buf;
+    LexerState state;
+    scheat::Scheat *host;
+public:
+    Lexer(scheat::Scheat*);
+    void lex(std::ifstream);
+    void addToken();
+};
+
 
 #endif /* Lexer_hpp */
