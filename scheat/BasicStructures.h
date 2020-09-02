950b2dd78249642c01c7b294044d4ac96885fc09
KazuchikaNakagawa <kaz04@me.com>
2020-09-02 20:54:58 +0900

    coding... some structure moved to other files.

--- scheat/BasicStructures.h
+++ scheat/BasicStructures.h
@@ -14,6 +14,121 @@ namespace scheat {
 
 class Context;
 
+
+enum class TokenKind : int {
+    
+    val_identifier,
+    val_num,
+    val_str,
+    val_double,
+    val_bool,
+    val_operator,
+    
+    tok_this,
+    tok_that,
+    tok_is,
+    tok_period,
+    tok_comma,
+    tok_range,
+    tok_paren_l,
+    tok_paren_r,
+    tok_external,
+    
+    embbed_func_print,
+    embbed_func_import,
+    embbed_func_free,
+    embbed_func_assemble,
+};
+
+union TokenValue {
+    std::string strValue;
+    int intValue;
+    bool boolValue;
+    double doubleValue;
+    TokenValue(){
+        doubleValue = 0.0;
+    }
+    ~TokenValue(){
+        
+    }
+    TokenValue(TokenValue &&){
+        
+    }
+    TokenValue(const TokenValue &){
+        
+    }
+};
+
+/// Token --Scheat's token.
+struct Token {
+    /// Token has chain structure
+    
+    // next : next Token pointer
+    Token *next;
+    
+    // prev : previous Token pointer
+    Token *prev;
+    
+    // TokenKind : presents type of Token.
+    TokenKind kind;
+    
+    // TokenValue: value has 4 types.
+    // scheat.int (llvm i32)
+    // scheat.str (llvm i8*) or (llvm %scheatstd.string)
+    // scheat.bool (llvm i1)
+    // scheat.double (llvm double)
+    TokenValue value;
+    
+    // last : returns last token
+    Token *last();
+    
+    // first : returns first token
+    Token *first();
+    
+    // location: SourceLocation
+    // column , line
+    SourceLocation location;
+    void valInt(std::string);
+    void valStr(std::string);
+    void valBool(std::string);
+    void valDouble(std::string);
+    static Token *add(Token *, Token *);
+    void enumerate();
+    std::string encodeOperator();
+    void out();
+    Token(){
+        next = nullptr;
+        prev = nullptr;
+        kind = TokenKind::val_num;
+        value.intValue = 0;
+        location = SourceLocation();
+    }
+    Token(Token &&) {};
+    Token(const Token &) {};
+};
+
+class Tokens {
+    Token *tokens;
+    Token *seek_ptr;
+public:
+    Token *ptr() { return seek_ptr; };
+    void skip();
+    void back();
+    Tokens *operator += (Token *rhs){
+        rhs->prev = this->tokens->last();
+        this->tokens->last()->next = rhs;
+        tokens = tokens->next;
+        return this;
+    };
+    Token *operator[](unsigned int index){
+        Token *t = tokens->first();
+        for (int i = 0; i != index; i++) {
+            t = t->next;
+        }
+        return t;
+    };
+};
+
 namespace basicStructs{
 
 class IRStream {
