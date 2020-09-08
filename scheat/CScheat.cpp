965cfee74fdef235faa66e8c43804ee86d2bc6ce
KazuchikaNakagawa <kaz04@me.com>
2020-09-08 16:29:33 +0900

    coding...

--- scheat/CScheat.cpp
+++ scheat/CScheat.cpp
@@ -8,3 +8,9 @@
 #include <stdio.h>
 #include "CScheat.h"
 
+
+bool scheat_build(const char *outputPath, const char *option, const char *sourceFilePath, const char *headerSearchPath, const char *librarySearchPath){
+    printf("this feture is not available yet.\n");
+    exit(0);
+    return false;
+}
